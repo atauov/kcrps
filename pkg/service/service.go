3cf450f680ed0404c69aa38dbb822a29a08ea08f
atauov <atauov@gmail.com>
2023-11-13 19:06:27 +0600

    middleware

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user dashboard.User) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type Invoice interface {
